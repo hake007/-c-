6994f3ebb3e91c9fcef7514bada7357981bba2e9
Zou <42364891+hake007@users.noreply.github.com>
2018-09-10 21:47:24 +0800

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,13 @@
+/*------------------------------
+ this is a hello world program
+ Author:zou l f
+ Date:2018 9.5
+---------------------------------*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+ 	  cout<<"hello world"<<endl;
+ 	  return 0;
+}
