3eec707b49ada53ad624059e8a7835a38dde87b7
Zou <42364891+hake007@users.noreply.github.com>
2018-09-10 21:48:41 +0800

    Add files via upload

---
+++ 加法计算器.cpp
@@ -0,0 +1,15 @@
+/* this is a ¼Ó·¨¼ÆËãÆ÷
+Author:zou lf
+Date:2018.9.5    */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cin>>a>>b;
+	c=a+b;
+	cout<<c<<endl;
+	return 0;
+}
+ 
