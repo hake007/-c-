5f369de36e1b4380056c899b1dd6446e6e74a3a6
Zou <42364891+hake007@users.noreply.github.com>
2018-09-10 21:50:20 +0800

    Add files via upload

---
+++ 生日卡片.cpp
@@ -0,0 +1,21 @@
+/*ÉúÈÕ¿¨Æ¬
+  Author:zou lf
+  Date  :2018 9.5 
+ */
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+ 	char name1[50];
+ 	char name2[50];
+ 	cin.getline(name1,50);
+ 	cin.getline(name2,50);
+ 	cout<<"#######################################"<<endl;
+ 	cout<<name1<<endl;
+ 	cout<<endl;
+ 	cout<<"Happy birthday to you!"<<endl;
+ 	cout<<endl;
+ 	cout<<   "sincerely yours"  <<name2<<endl;
+ 	cout<<"#######################################"<<endl;
+ 	return 0;
+ }
